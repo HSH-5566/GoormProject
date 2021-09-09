4caf929f7212396768e917ed5d8788ed09bebd37
한서현 <cat5566@kyonggi.ac.kr>
2021-09-09 12:29:05 +0000

    단어 바꾸기 함수 제작

---
+++ index.js
@@ -0,0 +1,30 @@
+const textarea = document.getElementById('text');
+const changeWord = document.getElementById('changed-word');
+const targetWord = document.getElementById('target-word');
+const submit = document.getElementById('submit');
+const result = document.getElementById('new-text');
+
+function changeText(text, word, newWord){
+	const changedText = text.replaceAll(word, newWord);
+	return changedText;
+}
+
+function changedText2(text, word, newWord){
+	const words = text.split(" ");
+	//람다함수 "배열내장함수"
+	const changedWords = words.map(_word =>{
+		return _word === word ? newWord : _word;
+	})
+	const changeText = changedWords.join(" ");
+	return changeText;
+}
+
+const onClickSubmit = (e) => {
+	const text = textarea.value;
+	const word = targetWord.value;
+	const newWord = changeWord.value;
+	
+	result.value = changeText(text, word, newWord);
+}
+
+submit.addEventListener('click', onClickSubmit);
